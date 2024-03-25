ef4b0de46dd76b479e7d017befd5b530780def6d
alves-cs <129676567+alves-cs@users.noreply.github.com>
2024-03-25 08:17:18 -0300

    aula-10

---
+++ 02-aula-10/index.js
@@ -0,0 +1,10 @@
+let nome = "Cintia"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome:")
+//let confirmacao = window.confirm("Confirma essa ação?")
+
